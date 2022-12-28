c62364b854414bef1f0bff47001c56481940c1c7
C V Anish <mahadevrao77@gmail.com>
2022-12-28 18:19:42 +0530

    HomePage

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Categories from './components/categories/categories.component';
+import React from 'react';
+
+const App = () => {
+  const categories=[
+    {
+      "id": 1,
+      "title": "hat's",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jacket's",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneaker's",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "women's",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "men's",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+  
+  return (
+    <Categories categories={categories}/>
+  );
+}
+
+export default App;
