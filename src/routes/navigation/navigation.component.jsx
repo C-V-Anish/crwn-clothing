74b7b5fd723d378ee5e6311bec970c6f9c7be96c
C V Anish <mahadevrao77@gmail.com>
2023-02-15 03:48:47 +0530

    Cart Functionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as Crwnlogo } from '../../assets/crown.svg'
 import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/fiebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const {currentUser}=useContext(UserContext);
@@ -19,7 +21,9 @@ const Navigation = () => {
         <div className="nav-links">
             <Link className="nav-link" to='/shop'>SHOP</Link>
             {currentUser ? <span className="nav-link" onClick={signOutUser}>{' '}SIGN OUT{' '}</span>:<Link className="nav-link" to='/auth'>SIGN IN</Link> }
+            <CartIcon/>
         </div>
+        <CartDropdown/>
       </div>
       <Outlet/>
     </Fragment>
